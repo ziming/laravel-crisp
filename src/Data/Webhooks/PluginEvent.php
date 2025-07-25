faefa3e867917afe4748f63008b2eae71e55a993
ziming <ziming@users.noreply.github.com>
2025-07-26 00:28:43 +0800

    add beta plugin event dto (my internal use only)

---
+++ src/Data/Webhooks/PluginEvent.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ziming\LaravelCrisp\Data\Webhooks;
+
+use Spatie\LaravelData\Data;
+
+/**
+ * @internal Not Ready Yet
+ */
+final class PluginEvent extends Data
+{
+    public function __construct(
+        public readonly string $website_id,
+        public readonly string $event,
+        public readonly array $data,
+        public readonly int $timestamp,
+    ) {}
+}
